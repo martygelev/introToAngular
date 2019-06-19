61626117dea7500af7e30b29774facb9441f5eb5
Martin Gelev <martygelev@gmail.com>
2019-06-19 11:08:28 +0300

    components:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,40 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
   <div class = 'app'>
-    {{title}}
+    {{ title + '!!!' }}
+    <h1> {{ title + 'title in h1 tag ' }}</h1>
+    <h1 [innerHTML] = "title"></h1>
+    <div> 
+      {{ numberOne }}
+      <br>
+      {{ numberTwo + numberOne }}
+
+      <img [src] = "img">
+      
+      <input type="text" [value] = "name" >
+
+      <div>
+          {{ name }}
+      </div>
+    </div>
+
+
+    <div>
+      {{ isHappy ? ':)' : ':(' }}
+    </div>
+
   </div>
   `
 })
 
 export class AppComponent{
   title: string
+  numberOne: number =1;
+  numberTwo: number =2;
+  isHappy: boolean = true;
+  img: string = 'favicon.ico';
+  name: string = 'Martin';
+
   constructor(){
     this.title = 'Ultimate Angular';
   }
